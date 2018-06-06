65cfc3d260699a98a179abc382193aeaceb277e8
Calleen D. Jones <calleendjones@gmail.com>
2018-06-05 23:50:04 -0400

    First Commit

---
+++ Lab26_ASP.NET_IDENTITY/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab26_ASP.NET_IDENTITY.MvcApplication" Language="C#" %>
---
+++ Lab26_ASP.NET_IDENTITY/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab26_ASP.NET_IDENTITY.Startup))]
+namespace Lab26_ASP.NET_IDENTITY
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
